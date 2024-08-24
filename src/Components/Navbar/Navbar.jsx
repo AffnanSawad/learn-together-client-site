a5e2e2bb1442eaecafa07a3b72455952827f2328
Affnan Sawad <affnansawad2002@gmail.com>
2024-08-24 13:08:39 +0600

    navbar,banner,footer added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,48 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
+
+   
+    const NavLink = <>
+    
+    <li className="font-bold"> <Link Link to="/">Home </Link>  </li>
+
+
+    <li className="font-bold"> <Link Link to="/assignments">Assignments </Link>  </li>
+
+
+
+    <li className="font-bold"> <Link Link to="/creating"> Create Assignments </Link>  </li>
+
+
+    <li className="font-bold"> <Link Link to="/pending">Pending Assignments </Link>  </li>
+
+
+
+    <li className="font-bold"> <Link Link to="/login">Log In </Link>  </li>
+    
+    
+    </>
+
+
+
+
+
+
+
+
+
+
+
+
     return (
-        <div className="navbar bg-base-100">
-        <div className="navbar-start">
+        <div className="navbar bg-base-100 ">
+        <div className="navbar-start h-1/2">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns="https://i.ibb.co/wK70v5C/download.png"
                 className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -22,38 +57,48 @@ const Navbar = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+             
+            
+            {NavLink}
+
+
+
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+        
+       <div className="flex">
+
+       
+
+
+<a className="btn btn-ghost font-bold  text-2xl">Learn Together</a>
+
+       </div>
+
+          
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
+            
+
+          {NavLink}
+
+              
           </ul>
         </div>
+   
+
+
+
+
+
+
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn btn-accent text-white">Log In</a>
         </div>
       </div>
+
+
     );
 };
 
